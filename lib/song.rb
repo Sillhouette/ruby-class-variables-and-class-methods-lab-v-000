87f51903629001311620ed502a4a32ddf5982760
Richard Melchior <raustin.melchior@gmail.com>
2018-06-05 04:28:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
 
   def self.artist_count
     artist_hash = {};
-    @@artists.map{|genre| artist_hash.has_key?(artist) ? artist_hash[artist] += 1 : artist_hash[artist] = 1}
+    @@artists.map{|artist| artist_hash.has_key?(artist) ? artist_hash[artist] += 1 : artist_hash[artist] = 1}
     return artist_hash;
   end
 
