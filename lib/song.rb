a3c2b63953bf23532d241f7f9ff07783a71bc3e2
Richard Melchior <raustin.melchior@gmail.com>
2018-06-05 03:55:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,10 @@ class Song
 
   attr_reader :name, :artist, :genre
 
+  @@count == 0;
+  @@artists = 0;
+  @@genres = 0;
+
   def initialize(name, artist, genre)
     @name = name;
     @artist = artist;
